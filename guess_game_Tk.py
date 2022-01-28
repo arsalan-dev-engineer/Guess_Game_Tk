1779d3c36db316ed61240fda6b8d7d37b9d98851
Arsalan <50211547+Azzy001@users.noreply.github.com>
2022-01-28 09:05:13 +0000

    Update guess_game_Tk.py
    This program is now complete 😃 I have added a message box, when users guess or lose then a message box will appear. A while loop has been added to limit the users guesses.   Moreover, after tries have ran out, entry and submit button will be disabled.

--- guess_game_Tk.py
+++ guess_game_Tk.py
@@ -1,21 +1,23 @@
 # Tkinter guessing game
 from tkinter import *
 import random
-
+from tkinter import messagebox
 
 window = Tk()
 
 # Bot Generator
-bot = random.randint(1, 20+1)
-print(bot)
+bot = random.randint(1, 10+1)
+
+# define outside of function to not overwrite each time
+tries = 5
 
 
 def submit():
-    tries = 5
-    while tries >= 0:
+    global tries  # to make tries a global variable
+    while tries > 0:  # this matches your tries amount correctly
         e1 = (int(guessE.get()))
         if e1 == bot:
-            print("You win")
+            messagebox.showinfo("Game Over", f"The number was {e1}, congratulations!")
             break
             
         elif e1 != bot:
@@ -23,13 +25,22 @@ def submit():
             tries -= 1
             break
 
+    # print status, disable the widgets
+    if tries == 0:
+        print("No more tries left")
+        guessE.configure(state="disabled")
+        submitBtn.configure(state="disabled")
+        over = messagebox.showerror("Game Over", f"The number was {e1}\nThank you for playing!")
+        window.destroy()
+        
 
+# Clear function will remove entries
 def clear():
     guessE.delete(0, "end")
     
     
 # Guess Label
-guessL = Label(window, text="Guess a number (between 1 and 20}")
+guessL = Label(window, text="Guess a number (between 1 and 10}")
 guessL.place(x=75, y=50)
 
 # Guess Entry
@@ -44,7 +55,7 @@ submitBtn.place(x=85, y=115)
 clearBtn = Button(window, width=10, height=2, text="Clear", command=clear)
 clearBtn.place(x=175, y=115)
 
-
+# Window Configs
 window.geometry("350x350")
 window.resizable(0, 0)
 window.attributes("-topmost", True)
