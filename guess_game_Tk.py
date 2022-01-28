b4e3ed54c1b679568da3ea4c045ec97e3386fb43
Arsalan <50211547+Azzy001@users.noreply.github.com>
2022-01-28 09:23:23 +0000

    Update guess_game_Tk.py
    Final update on the guess_game_Tk

--- guess_game_Tk.py
+++ guess_game_Tk.py
@@ -46,15 +46,15 @@ guessL = Label(window, text="Guess a number (between 1 and 10}")
 guessL.place(x=75, y=50)
 
 # Guess Entry
-guessE = Entry(window, width=25, bg="lightgrey")
+guessE = Entry(window, width=25, bg="#E3E3E3")
 guessE.place(x=95, y= 80)
 
 # Submit Button
-submitBtn = Button(window, width=10, height=2, text="Submit", command=submit)
+submitBtn = Button(window, width=10, height=2, text="Submit", command=submit, bg="#33CAFF")
 submitBtn.place(x=85, y=115)
 
 # Clear Button
-clearBtn = Button(window, width=10, height=2, text="Clear", command=clear)
+clearBtn = Button(window, width=10, height=2, text="Clear", command=clear, bg="#ABD7F1")
 clearBtn.place(x=175, y=115)
 
 # Window Configs
